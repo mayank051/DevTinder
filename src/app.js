5c906707e57e2645eb4597c0c9a61bcea94a55fb
Mayank <mayankraj051@gmail.com>
2024-10-16 14:09:37 +0530

    added example for query params & dynamic routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
+//query parameter
+app.get("/test", (req, res) => {
+  //For qurey parameter
+  console.log(req.query);
+  res.send("Hello From the test server !!");
+});
+
+//dynamic routes
+app.get("/testdynamics/:userid/:password", (req, res) => {
+  console.log(req.params);
+  res.send("Hello From the test Dynamic route !!");
+});
+
 app.get("/user", (req, res) => {
   res.send({
     firstName: "Mayank",
@@ -17,10 +30,6 @@ app.post("/user", (req, res) => {
 app.delete("/user", (req, res) => {
   //DB query to delete the user
   res.send("User Deleted successfully!");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello From the test server !!");
 });
 
 app.use("/", (req, res) => {
