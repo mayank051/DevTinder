449fbc19263fff04e05732ac0655bb446b2dd691
Mayank <mayankraj051@gmail.com>
2025-01-02 20:54:02 +0530

    Added feed API with pagination support

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter = express.Router();
 
 const USER_SAFE_DATA = [
@@ -59,6 +60,59 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.status(200).send({
       data,
     });
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    //Add pagination support from query params
+    const page = req.query.page || 1;
+    let limit = req.query.limit || 10;
+    if (limit > 50) limit = 50;
+
+    let skip = (page - 1) * limit;
+
+    //Find all the connection list(fromUserId, touserId) for loggedInUser (Interested/Ignored/Accepted/Rejected)
+    const connectionList = await ConnectionRequest.find({
+      $or: [
+        {
+          fromUserId: loggedInUser._id,
+        },
+        {
+          toUserId: loggedInUser._id,
+        },
+      ],
+    }).select(["fromUserId", "toUserId"]);
+
+    //Create a set that will contain all the userIds that needs to be hidden
+    const hiddenUserIds = new Set();
+    connectionList.forEach((connection) => {
+      hiddenUserIds.add(connection.fromUserId.toString());
+      hiddenUserIds.add(connection.toUserId.toString());
+    });
+
+    //Find all users that are not in the hiddenUserIds Array with pagination support
+    const feedUserList = await User.find({
+      $and: [
+        {
+          _id: { $nin: Array.from(hiddenUserIds) },
+        },
+        {
+          _id: { $ne: loggedInUser._id },
+        },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    const feedUserCount = feedUserList.length;
+
+    res.status(200).json({ value: feedUserList, count: feedUserCount });
   } catch (err) {
     res.status(400).send("Error: " + err.message);
   }
